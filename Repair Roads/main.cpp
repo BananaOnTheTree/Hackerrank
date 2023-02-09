c7d04fc87fe5198e100110d2f95e74a506ba298d
BananaOnTheTree <124771692+BananaOnTheTree@users.noreply.github.com>
2023-02-09 23:15:05 +0700

    WA

--- Repair Roads/main.cpp
+++ Repair Roads/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#define int long long
+//#define int long long
 #define pii pair<int, int>
 #define fi first
 #define se second
@@ -39,14 +39,59 @@ int MINUS(int x, int y)
     return (x - y) % MOD;
 }
 //========================================================================0========================================================================//
-
-
+int n, start, res;
+vector <int> adj[MAX];
+void input()
+{
+    cin >> n;
+    int u, v;
+    for (int i = 1; i <= n; i++) adj[i].clear();
+    for (int i = 1; i < n; i++)
+    {
+        cin >> u >> v;
+        u++; v++;
+        //cout << u << " " << v << '\n';
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if ((int)adj[i].size() == 1) start = i;
+    }
+}
+void dfs(int u, int p, int &res)
+{
+    bool direct = 0;
+    for (int v : adj[u])
+    {
+        if (v == p) continue;
+        if ((int)adj[v].size() == 1) continue;
+        res += direct;
+        direct = 1 - direct;
+        dfs(v, u, res);
+    }
+}
 main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     freopen("t.inp", "r", stdin);
-
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        input();
+        res = MOD;
+        int tmp = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            dfs(i, i, tmp);
+            minimize(res, tmp);
+            tmp = 1;
+        }
+        //dfs(start, start, res);
+        cout << res << '\n';
+    }
     return 0;
 }
